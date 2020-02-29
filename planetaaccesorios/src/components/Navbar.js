d8ab6f614152a7c62a581bd044099eb981b2a391
Radames Vaz <rgvtovar@hotmail.com>
2020-02-29 17:37:40 -0400

    playing with navbar

--- planetaaccesorios/src/components/Navbar.js
+++ planetaaccesorios/src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './navbar.css';
 import '../Container.css';
+import logo from '../assets/logo.jpg';
+
 
 function Navbar() {
     return(
@@ -10,9 +12,10 @@ function Navbar() {
                 <li><a href="#">Lo mas nuevo</a></li>
                 <li><a href="#">Galeria</a></li>
                 <li><a href="#">Home</a></li>
-                <li className="fl"><a href="#">Imagen aqui</a></li>
+                <div className="mw4 fl"> <a href="#"><img style={{zIndex: -1, marginRight: 0}} src={logo} /> </a> </div>
+                <li className="fl" style={{fontSize: 20}}><a href="#">Planeta Accesorios</a></li>
             </ul>
-            <img src="./assets/logo.png" />
+          
         </div>
     )
 }
