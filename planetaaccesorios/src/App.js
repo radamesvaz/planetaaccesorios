1ecc111bc23522ee1d6ba3fede30f230bf6e0ce1
Radames Vaz <rgvtovar@hotmail.com>
2020-03-01 19:01:54 -0400

    trying a different navbar

--- planetaaccesorios/src/App.js
+++ planetaaccesorios/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navegacion from './components/Navbar';
+import Navegaci2on from './components/2navbar';
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navegacion />
+      <Navegaci2on />
     </div>
   );
 }
--- planetaaccesorios/src/components/Navbar.js
+++ planetaaccesorios/src/components/Navbar.js
@@ -4,20 +4,20 @@ import '../Container.css';
 import logo from '../assets/logo.jpg';
 
 
-function Navbar() {
+
+function Navegacion() {
     return(
         <div className='navbar container'>
             <ul>
                 <li><a href="#">Contacto</a></li>
                 <li><a href="#">Lo mas nuevo</a></li>
                 <li><a href="#">Galeria</a></li>
-                <li><a href="#">Home</a></li>
+                <li><a href="#">Homee</a></li>
                 <div className="mw4 fl"> <a href="#"><img style={{zIndex: -1, marginRight: 0}} src={logo} /> </a> </div>
                 <li className="fl" style={{fontSize: 20}}><a href="#">Planeta Accesorios</a></li>
             </ul>
-          
         </div>
     )
 }
 
-export default Navbar;
+export default Navegacion;
